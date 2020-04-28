caf65f7821f443743f3ca3e7465e6273b3f36d99
Mark Yoon <mark.youngho.yoon@gmail.com>
2020-04-28 02:26:09 -0700

    main.py: main update 1
    - Drawing colors vertically instead of horizontally
    - Added img width/height logic and params
    - Scaled frame processing based on image size
    - Save image instead of displaying and add title

--- main.py
+++ main.py
@@ -4,27 +4,36 @@ import numpy as np
 import os
 from sklearn.cluster import KMeans
 
+PADDING = 50
+DEFAULT_WIDTH = 1200
+DEFAULT_HEIGHT = 400
+
 @click.command()
 @click.option('--video', help='File path for video to be processed.')
 @click.option('--videos', help='File directory for video to be processed.')
-@click.option('--dest', default='./', help='Directory location to save processed video.')
-def process_query(video, videos, dest):
+@click.option('--width', default=DEFAULT_WIDTH, help='Width of desired output image')
+@click.option('--height', default=DEFAULT_HEIGHT, help='Height of desired output image')
+def process_query(video, videos, width, height):
     if (video):
         videos = [video]
 
     for video in videos:
-        process_video(video)
+        process_video(video, int(width), int(height))
 
-def process_video(video_path):
+def process_video(video_path, width, height):
     video = cv2.VideoCapture(video_path)
+    total_frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     if (not video.isOpened()):
         click.echo("Error processing video {}.".format(video_path))
         return
 
     frame_count = 0
-    graph = np.zeros((1000, 1000, 3), dtype=np.uint8)
+    divisor = int(total_frame_count / (width - (PADDING * 2)))
+    graph = np.full((height, width, 3), 255, dtype=np.uint8)
     while (video.isOpened()):
         frame_count += 1
+        if frame_count % divisor != 0:
+            continue
         ret, frame = video.read()
         if ret:
             click.echo("Processing frame #{}".format(frame_count))
@@ -42,21 +51,23 @@ def process_video(video_path):
             hist = hist / hist.sum()
 
             colors = sorted([(fraction, color) for (fraction, color) in zip(hist, centroids)], reverse=True)
-            start = 0
+            draw_y_0 = PADDING
+            draw_x = int((frame_count / divisor) + PADDING)
             for (fraction, color) in colors:
                 print(color, "{:0.2f}%".format(fraction* 100))
-                end = start + (fraction * 1000)
-                cv2.rectangle(graph, (int(start), frame_count), (int(end), frame_count + 2),
+                draw_y_1 = draw_y_0 + (fraction * (height - (PADDING* 2)))
+                cv2.rectangle(graph, (draw_x, int(draw_y_0)), (draw_x + 1, int(draw_y_1)),
                         color.astype("uint8").tolist(), -1)
-                start = end
+                draw_y_0 = draw_y_1
             print("=================================================================")
 
         else:
             break
 
     graph = cv2.cvtColor(graph, cv2.COLOR_RGB2BGR)
-    cv2.imshow('Color graph', graph)
-    cv2.waitKey()
+    file_name = os.path.basename(video_path)
+    cv2.putText(graph, file_name, (PADDING, int(PADDING/2)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 1)
+    cv2.imwrite("processed_" + file_name + ".jpg", graph)
     video.release()
 
 if __name__ == '__main__':
