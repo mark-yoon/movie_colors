c7db253dc380fedf1c907ed193d2a93ef244c5e6
Mark Yoon <mark.youngho.yoon@gmail.com>
2020-04-30 18:23:51 -0700

    main.py: Add progress bar and clean up prints

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import click
+import tqdm
 import cv2
 import numpy as np
 import os
@@ -8,44 +9,41 @@ from sklearn.cluster import KMeans
 PADDING = 50
 DEFAULT_WIDTH = 1200
 DEFAULT_HEIGHT = 400
+DEFAULT_BUCKETS = 3
 
 @click.command()
 @click.option('--path', help='File path for file or dir of videos to be processed.')
 @click.option('--width', default=DEFAULT_WIDTH, help='Width of desired output image')
 @click.option('--height', default=DEFAULT_HEIGHT, help='Height of desired output image')
-def process_query(path, width, height):
+@click.option('--buckets', default=DEFAULT_BUCKETS, help='Number of top colors to track')
+def process_query(path, width, height, buckets):
     if os.path.isfile(path):
         files = [path]
     else:
         files = [path + f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
 
     for video in files:
-        process_video(video, int(width), int(height))
+        process_video(video, int(width), int(height), int(buckets))
 
-def process_video(video_path, width, height):
+def process_video(video_path, width, height, buckets):
     video = cv2.VideoCapture(video_path)
-    total_frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(total_frame_count)
     if (not video.isOpened()):
         click.echo("Error processing video {}.".format(video_path))
         return
 
-    frame_count = 0
+    total_frame_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
     divisor = int(total_frame_count / (width - (PADDING * 2)))
     graph = np.full((height, width+100, 3), 255, dtype=np.uint8)
-    while (video.isOpened()):
-        frame_count += 1
-        ret, frame = video.read()
-        if frame_count % divisor != 0:
-            continue
-        if ret:
-            click.echo("Processing frame #{}".format(frame_count))
+    for i in tqdm.tqdm(range(width - (PADDING * 2))):
+        video.set(cv2.CAP_PROP_POS_FRAMES, i * divisor - 1)
+        res, frame = video.read()
+        if res:
             # change colorspace
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             # reshape frame
             frame = frame.reshape((frame.shape[0] * frame.shape[1], 3))
             # get clusters and centroids
-            cluster = KMeans(n_clusters=5).fit(frame)
+            cluster = KMeans(n_clusters=3).fit(frame)
             centroids = cluster.cluster_centers_
 
             labels = np.arange(0, len(np.unique(cluster.labels_)) + 1)
@@ -55,14 +53,12 @@ def process_video(video_path, width, height):
 
             colors = sorted([(fraction, color) for (fraction, color) in zip(hist, centroids)], key=lambda tup: tup[0], reverse=True)
             draw_y_0 = PADDING
-            draw_x = int((frame_count / divisor) + PADDING)
+            draw_x = int(i + PADDING)
             for (fraction, color) in colors:
-                print(color, "{:0.2f}%".format(fraction* 100))
                 draw_y_1 = draw_y_0 + (fraction * (height - (PADDING* 2)))
                 cv2.rectangle(graph, (draw_x, int(draw_y_0)), (draw_x + 1, int(draw_y_1)),
                         color.astype("uint8").tolist(), -1)
                 draw_y_0 = draw_y_1
-            print("=================================================================")
 
         else:
             break
